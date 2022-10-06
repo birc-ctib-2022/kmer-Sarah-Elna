81df80653a4fc5db3596e62e13803c91963b7709
Sara-Elna <elna.elisa@gmail.com>
2022-10-06 11:26:00 +0200

    range error corrected

--- src/kmer.py
+++ src/kmer.py
@@ -12,7 +12,7 @@ def kmer(x: str, k: int) -> list[str]:
     ['AC', 'CG', 'GT', 'TC', 'CC']
     """
     result = []
-    for i in range(0, len(x)-k):
+    for i in range(0, len(x)-k+1):
         result.append(x[i:i+k])
     return result
 
@@ -43,14 +43,14 @@ def count_kmers(x: str, k: int) -> dict[str, int]:
     Computer all k-mers of x and count how often they appear.
 
     >>> count_kmers('tttaaatttaaa', 3)
-    {'ttt':2, 'tta':2, 'taa'=2, 'aaa'=2}
+    {'ttt': 2, 'tta': 2, 'taa': 2, 'aaa': 1, 'aat': 1, 'att': 1}
     """
     result = {}
     unique_kmer_list = unique_kmers(x, k)
     for i in range(0, len(unique_kmer_list)):
         search_kmer = unique_kmer_list[i]
         search_count = 0
-        for i in range(0, len(x)-k):
+        for i in range(0, len(x)-k+1):
             if search_kmer == x[i:i+k]:
                 search_count += 1
         result[search_kmer] = search_count
