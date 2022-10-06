4b2f17117336877f4b5dff0422d75a201f002597
Sara-Elna <elna.elisa@gmail.com>
2022-10-06 11:18:10 +0200

    First attempt

--- src/kmer.py
+++ src/kmer.py
@@ -8,24 +8,51 @@ def kmer(x: str, k: int) -> list[str]:
     >>> kmer('agtagtcg', 3)
     ['agt', 'gta', 'tag', 'agt', 'gtc', 'tcg']
 
-    FIXME: do you want more tests here?
+    >>> kmer('ACGTCC', 2)
+    ['AC', 'CG', 'GT', 'TC', 'CC']
     """
-    ...
+    result = []
+    for i in range(0, len(x)-k):
+        result.append(kmer[i:i+k])
+    return result
 
 
 def unique_kmers(x: str, k: int) -> list[str]:
     """
     Computer all unique k-mers of x.
 
-    FIXME: do you want more tests here?
+    >>> unique_kmers('agtagtcg', 3)
+    ['agt', 'gta', 'tag', 'gtc', 'tcg']
+
+    >>> unique_kmers('AAATAAGAAC', 2)
+    ['AA', 'AT', 'TA', 'AG', 'GA', 'AC']
     """
-    ...
+    result = []
+    kmer_list = kmer(x, k)
+    for i in range(0, len(kmer_list)):
+        if kmer_list[i] not in result:
+            result.append(kmer_list[i])
+        else:
+            continue
+    return result
+
 
 
 def count_kmers(x: str, k: int) -> dict[str, int]:
     """
     Computer all k-mers of x and count how often they appear.
 
-    FIXME: do you want more tests here?
+    >>> count_kmers('tttaaatttaaa', 3)
+    {'ttt':2, 'tta':2, 'taa'=2, 'aaa'=2}
     """
-    ...
+    result = {}
+    unique_kmer_list = unique_kmers(x, k)
+    for i in range(0, len(unique_kmer_list)):
+        search_kmer = unique_kmer_list[i]
+        search_count = 0
+        for i in range(0, len(x)-k):
+            if search_kmer == x[i:i+k]:
+                search_count += 1
+        result[search_kmer] = search_count
+    return result
+            
--- src/test_kmer.py
+++ src/test_kmer.py
@@ -2,5 +2,11 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-def test_me():
-    assert False, "Don't forget to write tests"
+def test_kmer():
+    assert kmer('accttga', 2) == ['ac', 'cc', 'ct', 'tt', 'tg', 'ga']
+
+def test_unique_kmers():
+    assert unique_kmers('accttaccga', 2) == ['ac', 'cc', 'ct', 'tt', 'ta', 'cg', 'ga']
+
+def test_count_kmers():
+    assert count_kmers('acctacct', 2) == {'ac':2, 'cc':2, 'ct':2, 'ta':1}
